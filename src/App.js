91648d0e76c7c121dcdc6b95679d07ea4319c80f
Bartłomiej Depciuch <depciuch322@gmail.com>
2022-07-18 04:28:54 +0200

    pierwsza wersja apki pogodowej

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+const api = {
+  key: "5548dc3776887cd60acb44353f9daf3a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const days = [
+    "Niedziela",
+    "Poniedziałek",
+    "Wtorek",
+    "Środa",
+    "Czwartek",
+    "Piątek",
+    "Sobota",
+  ];
+
+  const date = new Date();
+  const day = days[date.getDay()];
+  const hours = ("0" + date.getHours()).slice(-2);
+  const minutes = ("0" + date.getMinutes()).slice(-2);
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `${api.base}weather?q=${city}&units=metric&APPID=${api.key}&lang=pl`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setCity("");
+          console.log(result);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="search_box">
+        <input
+          type="text"
+          className="search_bar"
+          placeholder="Szukaj..."
+          onChange={(e) => setCity(e.target.value)}
+          value={city}
+          onKeyPress={search}
+        />
+      </section>
+
+      {typeof weather.main != "undefined" ? (
+        <section className="main_box">
+          <p className="main_daytime">
+            {day}, {hours}:{minutes}
+          </p>
+          <p className="main_city">
+            {weather.name}, {weather.sys.country}
+          </p>
+          <p className="main_temperature">{Math.round(weather.main.temp)}°C</p>
+          <p className="main_state">{weather.weather[0].description}</p>
+        </section>
+      ) : (
+        ""
+      )}
+    </main>
   );
 }
 
